71a59990eaba6fe288942ca4b61aca1e676b4dfc
Nnaemeka Ohakim <ohakim.nnaemeka@gmail.com>
2023-11-09 12:24:40 +0100

    Customized runtime format

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -6,8 +6,12 @@ type Movie struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"-"` // Use the directive to hide fields
 	Title     string    `json:"title"`
-	Year      int32     `json:"year,omitempty"`
-	Runtime   int32     `json:"runtime,omitempty"`
-	Genres    []string  `json:"genres,omitempty"`
-	Version   int32     `json:"version"`
+	// Use the Runtime type instead of int32. Note that the omitempty directive will
+	// still work on this: if the Runtime field has the underlying value 0, then it will
+	// be considered empty and omitted -- and the MarshalJSON() method we just made
+	// won't be called at all.
+	Year    int32    `json:"year,omitempty"`
+	Runtime Runtime  `json:"runtime,omitempty"`
+	Genres  []string `json:"genres,omitempty"`
+	Version int32    `json:"version"`
 }
