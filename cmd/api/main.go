b0a456ed0582952fd00f0322793e4281d373b67f
Nnaemeka Ohakim <ohakim.nnaemeka@gmail.com>
2023-11-18 16:21:29 +0100

    Setup Movies model

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/Nerdward/greenlight/internal/data"
 	_ "github.com/lib/pq"
 )
 
@@ -34,6 +35,7 @@ type config struct {
 type application struct {
 	config config
 	logger *log.Logger
+	models data.Models
 }
 
 func main() {
@@ -65,6 +67,7 @@ func main() {
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	// declare a HTTP server with some timeout settings, port number and servemux
