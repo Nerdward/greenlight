ab886e161324b277d9a8d43a8dfa708c612dcb92
Nnaemeka Ohakim <ohakim.nnaemeka@gmail.com>
2023-11-23 17:37:32 +0100

    Handled race conditions

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -115,18 +115,33 @@ func (m MovieModel) Update(movie *Movie) error {
 	query := `
 	UPDATE movies
 	SET title = $1, year= $2, runtime= $3, genres = $4, version = version + 1
-	WHERE id = $5
+	WHERE id = $5 AND version = $6
 	RETURNING version
 	`
 
 	// Create an args slice containing the values for the placeholder parameters.
-	args := []interface{}{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres), movie.ID}
+	args := []interface{}{
+		movie.Title,
+		movie.Year,
+		movie.Runtime,
+		pq.Array(movie.Genres),
+		movie.ID,
+		movie.Version}
 
 	// Use the QueryRow() method to execute the query, passing in the args slice as a
 	// variadic parameter and scanning the new version value into the movie struct.
 	err := m.DB.QueryRow(query, args...).Scan(&movie.Version)
 
-	return err
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (m MovieModel) Delete(id int64) error {
