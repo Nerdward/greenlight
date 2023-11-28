12924534722f2cb73780f02c4594d0380189d334
Nnaemeka Ohakim <ohakim.nnaemeka@gmail.com>
2023-11-28 11:46:24 +0100

    validated query parameters

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -21,6 +21,8 @@ func (app *application) routes() *httprouter.Router {
 
 	// Register methods, URL patterns and handler functions
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+
+	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
